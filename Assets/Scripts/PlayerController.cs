3f4a24981105b3cf4e6ace12a51cd234f6d4ba74
sitalbi <simon0111@live.fr>
2022-06-08 23:50:59 +0200

    first commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private int jumpForce, movementSpeed;
+
+    private Rigidbody2D rb2D;
+    
+    private bool jumpPressed, canMove;
+
+    [NonSerialized] public int direction;
+
+    void Start() {
+        rb2D = GetComponent<Rigidbody2D>();
+        direction = 1;
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump")) {
+            jumpPressed = true;
+        }
+    }
+
+    void FixedUpdate() {
+        
+        if (jumpPressed) {
+            rb2D.velocity = new Vector2(direction * movementSpeed, 10*jumpForce * Time.deltaTime);
+            jumpPressed = false;
+        }
+    }
+}
