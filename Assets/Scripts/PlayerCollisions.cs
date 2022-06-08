6e1d938c6b3361767ee0ec27649fa10792af37e2
sitalbi <simon0111@live.fr>
2022-06-09 00:34:20 +0200

    first moves implemented

--- Assets/Scripts/PlayerCollisions.cs
+++ Assets/Scripts/PlayerCollisions.cs
@@ -1,18 +1,36 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerCollisions : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private int wallLayers;
+    private PlayerController playerController;
+    
+    void Start() {
+        playerController = GetComponent<PlayerController>();
+    }
+
+    
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void OnCollisionEnter2D(Collision2D col) {
+        if (col.gameObject.layer == wallLayers) {
+            playerController.isOnWall = true;
+            playerController.xDirection *= -1;
+            playerController.SetGravityScale(0);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D col) {
+        if (col.gameObject.layer == wallLayers) {
+            playerController.isOnWall = false;
+            playerController.yDirection *= -1;
+            playerController.SetGravityScale(playerController.originalGravityScale);
+        }
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,17 +5,21 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private int jumpForce, movementSpeed;
+    [SerializeField] private int jumpForce, movementSpeed, wallSpeed;
 
     private Rigidbody2D rb2D;
     
     private bool jumpPressed, canMove;
-
-    [NonSerialized] public int direction;
+    
+    [NonSerialized] public float originalGravityScale;
+    [NonSerialized] public int xDirection, yDirection;
+    [NonSerialized] public bool isOnWall;
 
     void Start() {
         rb2D = GetComponent<Rigidbody2D>();
-        direction = 1;
+        xDirection = 1;
+        yDirection = 1;
+        originalGravityScale = rb2D.gravityScale;
     }
 
     
@@ -27,10 +31,24 @@ public class PlayerController : MonoBehaviour
     }
 
     void FixedUpdate() {
-        
+
         if (jumpPressed) {
-            rb2D.velocity = new Vector2(direction * movementSpeed, 10*jumpForce * Time.deltaTime);
+            rb2D.velocity = new Vector2(xDirection * movementSpeed * 50 * Time.deltaTime, 50 * jumpForce * Time.deltaTime);
             jumpPressed = false;
         }
+        
+        if (isOnWall) {
+            //Up or down depending on the wall
+            rb2D.velocity = new Vector2(rb2D.velocity.x, yDirection * wallSpeed);
+        }
+        
+        
+    }
+
+    public void SetGravityScale(float value) {
+        if (value == 0) {
+            rb2D.velocity = Vector2.zero;
+        }
+        rb2D.gravityScale = value;
     }
 }
