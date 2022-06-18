bfc884745c3c8c3eb753562aab7117cd79339713
sitalbi <simon0111@live.fr>
2022-06-18 12:46:30 +0200

    added main menu

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -11,10 +12,11 @@ public class GameManager : MonoBehaviour
     [SerializeField] public ObjectSpawner spawner;
     [SerializeField] private float minCoolDown, coolDownStep;
     [SerializeField] public int collectablePoints;
+    [SerializeField] private ObjectMovement objectMovement;
     
     [NonSerialized] public int score;
 
-    private bool coolDownChanged;
+    private bool coolDownChanged, isDead;
 
     void Start() {
         score = 0;
@@ -24,15 +26,17 @@ public class GameManager : MonoBehaviour
 
     void Update() {
         scoreText.text = score.ToString();
-
-        if (score != 0 && score % 10 == 0) {
-            if (spawner.obstacleCoolDown > minCoolDown && !coolDownChanged) {
-                spawner.obstacleCoolDown -= coolDownStep;
-                coolDownChanged = true;
-                if (spawner.spawnRate > 2) {
-                    spawner.spawnRate--;
+        if (score != 0) {
+            if (score % 10 == 0) {
+                if (spawner.obstacleCoolDown > minCoolDown && !coolDownChanged) {
+                    spawner.obstacleCoolDown -= coolDownStep;
+                    coolDownChanged = true;
+                    if (spawner.spawnRate > 2) {
+                        spawner.spawnRate--;
+                    }
                 }
             }
+            
             if (score > 35) {
                 spikeSpawner1.SetActive(true);
                 spikeSpawner2.SetActive(true);
@@ -41,12 +45,19 @@ public class GameManager : MonoBehaviour
         else {
             coolDownChanged = false;
         }
+
+        if (isDead) {
+            if (Input.GetButtonDown("Jump")) {
+                SceneManager.LoadScene("Scenes/MainMenu");
+            }
+        }
     }
 
     public void Death() {
         Destroy(player);
         Time.timeScale = 0;
         panel.SetActive(true);
-        loseText.text = "Lose !";
+        loseText.text = "Lost !";
+        isDead = true;
     }
 }
